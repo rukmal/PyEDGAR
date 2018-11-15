5e9b1629ec359cef0b630214b93b8bee3c816f35
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-11-15 18:52:10 -0500

    Added functionality to retrieve filings by type, and dedicated functions for 10-K and 10-Q filings

--- PyEDGAR/filings/__init__.py
+++ PyEDGAR/filings/__init__.py
@@ -1,1 +1,3 @@
 from .filings import getAllFilings
+from .filings import get10K
+from .filings import get10Q
--- PyEDGAR/filings/filings.py
+++ PyEDGAR/filings/filings.py
@@ -5,14 +5,11 @@ from .downloader import __downloadFilings
 from .parser import __parseHTML
 
 
-def getAllFilings(cik: str, includeRaw: bool=False) -> list:
+def getAllFilings(cik: str) -> list:
     """Function to get a list of SEC filings, given a company CIK.
     
     Arguments:
         cik {str} -- CIK of the target company.
-    
-    Keyword Arguments:
-        includeRaw {bool} -- Flag to include raw HTML (default: {False}).
     
     Returns:
         list -- Structured list of dictionaries with filing information for a
@@ -27,7 +24,54 @@ def getAllFilings(cik: str, includeRaw: bool=False) -> list:
     # Parsing HTML
     filings_parsed = __parseHTML(pages_html=pages_html)
 
-    # Raw page HTML
-    if includeRaw: filings_parsed['_raw'] = pages_html
-
     return filings_parsed
+
+
+def getFilingByType(cik: str, filing_type: str) -> list:
+    """Function to get filings by type, for a given company CIK.
+    
+    Arguments:
+        cik {str} -- CIK of target company.
+        filing_type {str} -- Target filing type.
+    
+    Returns:
+        list -- Structured list of dictionaries with target filing information.
+    """
+
+    # Getting all filings
+    filings = getAllFilings(cik=cik)
+
+    # Separating target filings
+    target_filings = list()
+
+    for filing in filings:
+        if filing_type in filing['type']:
+            target_filings += [filing]
+    
+    return target_filings
+
+
+def get10K(cik: str) -> list:
+    """Function to get 10-K filings for a target company CIK.
+    
+    Arguments:
+        cik {str} -- CIK of target company.
+    
+    Returns:
+        list -- Structured list of dictionaries with 10-K filings.
+    """
+
+    return getFilingByType(cik=cik, filing_type='10-K')
+
+
+def get10Q(cik: str) -> list:
+    """Function to get 10-Q filings for a target company CIK.
+    
+    Arguments:
+        cik {str} -- CIK of target company.
+    
+    Returns:
+        list -- Structured list of dictionaries with 10-Q filings.
+    """
+
+    return getFilingByType(cik=cik, filing_type='10-Q')
