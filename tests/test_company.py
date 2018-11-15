a6c5034573128572c723a04ce3136f3eaddd2b9b
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-11-14 23:17:14 -0500

    Added test to check phone number extraction for coverage

--- tests/test_company.py
+++ tests/test_company.py
@@ -14,14 +14,44 @@ class TestEdgarCompany(unittest.TestCase):
         """
 
         # Test variables
-        candidate_ticker = 'AAPL'
-        candidate_cik = PyEDGAR.util.getCIK(ticker=candidate_ticker)
+        test_cik = '0000320193'
 
         # Getting company info
-        info = PyEDGAR.company.getInfo(cik=candidate_cik)
+        info = PyEDGAR.company.getInfo(cik=test_cik)
 
-        # Expected results
+        # Expected result
         expected_types = ['Mailing Address', 'Business Address']
+        # Candidate result
         candidate_types = [i['type'] for i in info['addresses']]
 
+        # Check equality
         self.assertTrue(sorted(expected_types) == sorted(candidate_types))
+
+
+    def test_companyPhone(self):
+        """Test the company module address retrieval from `getInfo` in the
+        `company` module.
+
+        Test case uses ticker 'TSLA', with the corresponding CIK
+        '0001318605'. Verifies that the Business address contains the phone
+        number '6506815000'.
+        """
+
+        # Test variables
+        test_cik = '0001318605'
+
+        # Getting company info
+        info = PyEDGAR.company.getInfo(cik=test_cik)
+
+        # Expected result
+        expected_phone = '6506815000'
+
+        # Getting candidate result
+        candidate_phone = ''
+        for address in info['addresses']:
+            if address['type'] == 'Business Address':
+                candidate_phone = address['phone']
+                break
+        
+        # Check numbers match
+        self.assertEqual(candidate_phone, expected_phone)
