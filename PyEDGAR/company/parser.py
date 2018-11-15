73e27cbc99486b73a958b85634a557f229f23cba
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-11-15 00:36:56 -0500

    Added functionality to extract former company name from parsed EDGAR HTML

--- PyEDGAR/company/parser.py
+++ PyEDGAR/company/parser.py
@@ -21,9 +21,10 @@ def __parseHTML(page_html: str) -> dict:
 
     # Getting company addresses
     company_info['addresses'] = __getAddresses(parsed=parsed)
-
     # Getting company name
     company_info['name'] = __getCompanyName(parsed=parsed)
+    # Getting former company names
+    company_info['former_names'] = __getFormerNames(parsed=parsed)
 
     return company_info
 
@@ -38,6 +39,7 @@ def __getAddresses(parsed: BeautifulSoup) -> list:
         list -- List of addresses.
     """
 
+    # Addresses container
     address_divs = parsed.find_all('div', class_='mailer')
 
     # Building RegEx for phone number
@@ -104,3 +106,40 @@ def __getCompanyName(parsed: BeautifulSoup) -> str:
 
     # Getting name (first raw text instance)
     return name_raw_text[0].strip()
+
+
+def __getFormerNames(parsed: BeautifulSoup) -> list:
+    """Function to extract former company names, and dates through which reports
+    were filed under that name from the parsed HTML EDGAR page.
+    Searches for strings matching format for previous names first, then extracts
+    former name and filings-through date separately.
+    
+    Arguments:
+        parsed {BeautifulSoup} -- Parsed HTML from company EDGAR filing.
+    
+    Returns:
+        list -- List of former names (if any), empty list otherwise.
+    """
+
+    # Former names container
+    former_container = parsed.find('p', class_='identInfo')
+
+    # List for former names
+    former_names = list()
+
+    # Building RegEx for former name sentence
+    former_sentence_re = re.compile(r'(formerly:.+?\(filings through .+?\))')
+    # Getting sentence matches
+    former_sentences = former_sentence_re.findall(former_container.text)
+    
+    # Building RegEx for name and filings-through date extraction
+    name_and_date_re = re.compile(r'formerly:(.*)\(.*(\d{4}-\d{2}-\d{2})')
+    # Extracting former name and filings-through date for each sentence
+    for sentence in former_sentences:
+        matches = name_and_date_re.findall(sentence)
+        former_name = dict()
+        former_name['former_name'] = matches[0][0].strip()
+        former_name['filings_through'] = matches[0][1]
+        former_names += [former_name]
+
+    return former_names
