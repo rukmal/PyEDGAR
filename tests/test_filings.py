4e890dc5fbf69599be7eca38b122e82d201b3613
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-11-15 18:06:27 -0500

    Added test for filings retrieval

--- tests/test_filings.py
+++ tests/test_filings.py
@@ -4,18 +4,21 @@ import unittest
 
 
 class TestEdgarFilings(unittest.TestCase):
-    """Test the `filings` in the `edgar` module.
+    """Test the `filings` module.
     """
 
-    def test_getFilingsIndex(self):
+    def test_getFilingsList(self):
         """Test getting filings index from EDGAR.
 
-        This test uses the ticker 'AAPL'.
+        This test case uses the ticker 'AAPL', with corresponding CIK
+        '0000320193'. Verifies that over 550 filings are retrieved.
         """
 
         # Test variables
-        candidate_ticker = 'AAPL'
-        candidate_cik = PyEDGAR.util.getCIK(ticker=candidate_ticker)
+        candidate_cik = '0000320193'
 
-        # Getting filings (temporary)
-        PyEDGAR.filings.getAllFilings(cik=candidate_cik)
+        # Getting filings
+        candidate_filings = PyEDGAR.filings.getAllFilings(cik=candidate_cik)
+
+        # Verifying over 550 filings were retrieved
+        self.assertGreater(len(candidate_filings), 550)
